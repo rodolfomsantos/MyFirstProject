bdd01cff5169ce016f8280d386023fd5afec0e57
rodolfomsantos <rodolfo.m.santos@gmail.com>
2017-03-16 21:10:27 +0000

    for Loop

---
+++ src/Loops.java
@@ -0,0 +1,46 @@
+
+public class Loops {
+	public static void main(String[] args) {
+
+		//whileLoop();
+		//doWhileLoop();
+		forLoop();
+
+	}
+
+	static void whileLoop (){
+
+		int counter = 10;
+
+		while (counter < 10) {
+
+			counter = counter + 1;
+			System.out.println(counter);
+		}
+
+	}
+	
+	static void doWhileLoop() {
+		
+		int counter = 10;
+		do {
+			
+			counter = counter + 1;
+			System.out.println(counter);
+			
+			} while (counter < 10); {
+			
+		}
+		
+	}
+	
+	static void forLoop() {
+		// for (initialize counter; condition to see if we should run the loop; change counter variable)
+		
+		for (int counter = 0; counter <= 10; counter = counter + 1){
+			System.out.println(counter);
+		}
+		
+	}
+
+}
