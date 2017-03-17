e7411f032c20ed5e789f2c0ea4c29cb9fb455f7f
rodolfomsantos <rodolfo.m.santos@gmail.com>
2017-03-17 21:07:53 +0000

    Classes and Objects

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,19 +1,19 @@
-import javax.swing.JLabel;
-
 public class ClassesAndObjects {
 
 	public static void main(String[] args) {
 		
-		JLabel label = new JLabel();
-		Person john = new Person();
-		john.name = "John";
-		john.age = 20;
-		Person bob = new Person();
-		bob.name = "Bob";
-		bob.age = 25;
+		Person john = new Person("John");
+		john.setName("John");
+		john.setAge(20);;
+		Person bob = new Person("Bob");
+		bob.setName("Bob");
+		bob.setAge(25);
 		
-		System.out.println(john.name + " is "+ john.age + " years old." );
-		System.out.println(bob.name + " is "+ bob.age + " years old." );
+		john.sayHelloTo (bob);
+		bob.sayHelloTo(john);
+		
+		System.out.println(john.getName() + " is "+ john.getAge() + " years old." );
+		System.out.println(bob.getName() + " is "+ bob.getAge() + " years old." );
 		
 	}
 	
--- src/Person.java
+++ src/Person.java
@@ -5,4 +5,31 @@ public class Person {
 	String name; // instance variable
 	int age; // if we make "satic int age;" this will fix the age to the last integer of the class whe its called.(é como a funçâo $ no excel, ou seja fixa o valor mas aqui ao ultimo valor introduzido quando se chama esta classe)
 	
+	public Person (String name) {
+		this.name = name;
+		System.out.println("Person Created");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void sayHelloTo(Person person) {
+		
+		System.out.println(getName() + " said hello to " + person.getName());
+		
+	}
+	
 }
