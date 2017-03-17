69e9eadc38fcddd2ff2c58c8d50af829f7b0292d
rodolfomsantos <rodolfo.m.santos@gmail.com>
2017-03-17 19:29:27 +0000

    Objects

---
+++ src/Objects.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Objects {
+	
+	public static void main(String[] args) {
+		
+		//int number = 5;
+		// create our window
+		JFrame window = new JFrame(); // window variable that holds JFrame objects, and we're creating a new JFrame to assign to our variable
+		window.setTitle("My Window"); // we're calling objects on our JFrame object to configure it
+		window.setSize(800, 600);
+		window.setVisible(true);
+		
+		JLabel label = new JLabel(); // label variable holds JLabel objects, and we're creating a new JLabel to assign to our variable
+		label.setText("My Label"); // we're calling the setText method on our JLabel object to set its text property
+		
+		window.add(label); // we're calling the add method of our JFrame object to add our label to the window
+	}
+}
