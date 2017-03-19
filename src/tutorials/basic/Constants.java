f9c218c68ab4c7f173962c63867173e43bca643b
rodolfomsantos <rodolfo.m.santos@gmail.com>
2017-03-19 11:32:09 +0000

    Constants

--- src/tutorials/basic/Constants.java
+++ src/tutorials/basic/Constants.java
@@ -1,8 +1,21 @@
 package tutorials.basic;
 
 public class Constants {
+	
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
+	public static final Person P1 = new Person("");
+	public static final int NUM_ENEMIES = 5;
 
 	public static void main(String[] args) {
+		
+		// constants never change once a value is assigned
+		// usually static
+		// by convention, constants are upper case
+		// declare constants by using the "final" modifier
+		// constants can be public because there's no harm in accessing variables directly if you can't alter it 
+		
+		System.out.println(Math.PI);
 		
 	}
 	
